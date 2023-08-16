96b40e00ae29601670d678f2070fc01303b53bd4
TessaHayden <tessahayden3@gmail.com>
2023-08-16 16:21:13 -0700

    Fixing bugs - work in progress.

--- src/index.js
+++ src/index.js
@@ -1,28 +1,46 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
-import CurrencyService from './currency-service'
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/styles.css";
+import CurrencyService from "./currency-service";
 
 function getExchangeRate(currency) {
   let promise = CurrencyService.getExchangeRate(currency);
-  promise.then(function (currencyArray) {
-    printElements(currencyArray);
-  }, function (errorArray) {
-    printError(errorArray);
-  });
+  promise.then(
+    function (currencyArray) {
+      printElements(currencyArray);
+      conversionFunc();
+    },
+    function (errorArray) {
+      printError(errorArray);
+    }
+  );
 }
 
-function printElements(data) {
+function printElements() {
+  let input = "input";
   document.querySelector(
     "#showResponse"
-  ).innerText = `Here is the exchange rate from USD to ${data.conversion_rates}`;
+  ).innerText = `Here is the exchange rate from USD to ${input}`;
 }
 function printError(error) {
   document.querySelector(
     "#showResponse"
   ).innerText = `There was an error accessing the weather data for ${error[2]}: ${error[0].status} ${error[0].statusText}: ${error[1].message}`;
 }
-
+function conversionFunc(promise) {
+  if (promise.result === "success") {
+    console.log("working");
+  } else {
+    const htmlOutput = promise.conversion_rates
+      .map(
+        (el) =>
+          `<li>${el.conversion_rates}\n<p>-- ${el.conversion_rates} --</p></li>`
+      )
+      .join("");
+    const list = document.getElementById("currency-abb");
+    list.innerHTML = htmlOutput;
+  }
+}
 function handleFormSubmission(event) {
   event.preventDefault();
   const currencyType = document.querySelector("#currency-type").value;
@@ -31,7 +49,7 @@ function handleFormSubmission(event) {
 }
 
 window.addEventListener("load", function () {
-  this.document
+  document
     .querySelector("form")
     .addEventListener("submit", handleFormSubmission);
 });
